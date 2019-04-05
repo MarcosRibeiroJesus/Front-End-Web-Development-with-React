4b82c99195e571364e87af3a481984bdc322c9f3
MarcosRibeiroJesus <mrj84@outlook.com>
2019-04-05 16:27:22 -0300

    Components part 2

--- Week 1/confusion/src/App.js
+++ Week 1/confusion/src/App.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from './components/MenuComponent';
+import { DISHES } from './components/shared/dishes/dishes';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,7 +23,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }

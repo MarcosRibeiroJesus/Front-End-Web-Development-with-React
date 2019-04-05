261e644048b6e586dd9686771f11591efa23e20b
MarcosRibeiroJesus <mrj84@outlook.com>
2019-04-05 15:54:02 -0300

    Components part 1

---
+++ Week 1/confusion/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from './components/MenuComponent';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
+    );
+  }
+}
+
+export default App;
